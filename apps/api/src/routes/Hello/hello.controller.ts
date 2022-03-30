6c7147113b7b8be62b6dd02246d303b50c0b24c7
pedrobzz <pedrobzz66@gmail.com>
2022-03-29 23:14:50 -0300

    fix(base route): Removed a joke kk

--- apps/api/src/routes/Hello/hello.controller.ts
+++ apps/api/src/routes/Hello/hello.controller.ts
@@ -6,7 +6,7 @@ const helloRoute = Router();
 
 helloRoute.get("/", (req, res) => {
   res.status(200).json({
-    message: "Hello, World! React Query!!!! hahahaha deryck 1!",
+    message: "Hello, GraphQL!",
   });
 });
 
