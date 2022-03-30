bed2645f54a72c6182426b4fc8c6d4cee50a6bb8
pedrobzz <pedrobzz66@gmail.com>
2022-03-29 23:13:51 -0300

    fix(API): Now the API will try  three times connect into the database before giving up and breaking

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import helloRoute from "./routes/Hello/hello.controller";
 import cors from "cors";
-import { MikroORM } from "@mikro-orm/core";
+import { Connection, IDatabaseDriver, MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./config/constants";
 
 import mikroOrmConfig from "./mikro-orm.config";
 
-import { ApolloServer } from "apollo-server-express";
+import { ApolloServer, ExpressContext } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { PostResolver } from "./resolvers/PostResolver";
 
@@ -17,23 +17,39 @@ app.use(cors());
 app.use("/", helloRoute);
 
 const main = async () => {
-  const orm = await MikroORM.init(mikroOrmConfig);
-  await orm.getMigrator().up();
+  console.info("[environment: ", process.env.NODE_ENV, "]");
+  let tries = 5;
+  let apolloServer: ApolloServer<ExpressContext>;
+  let orm: MikroORM<IDatabaseDriver<Connection>>;
+  while (tries) {
+    console.log("Tries: ", tries);
+    try {
+      orm = await MikroORM.init(mikroOrmConfig);
+      console.log("MikroORM Initialized");
+      await orm.getMigrator().up();
 
-  const apolloServer = new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [PostResolver],
-      validate: false,
-    }),
-    context: () => ({
-      em: orm.em,
-    }),
-  });
-  await apolloServer.start();
+      apolloServer = new ApolloServer({
+        schema: await buildSchema({
+          resolvers: [PostResolver],
+          validate: false,
+        }),
+        context: () => ({
+          em: orm.em,
+        }),
+      });
+      await apolloServer.start();
+      break;
+    } catch {
+      await new Promise(r => setTimeout(r, 5000));
+      tries -= 1;
+      console.log("Tries: ", tries);
+    }
+  }
+
   apolloServer.applyMiddleware({ app });
 
-  const server = app.listen(3003, () => {
-    console.log("listening at 3003!");
+  const server = app.listen(4000, () => {
+    console.log("listening at 4000!");
   });
   return { orm, server };
 };
